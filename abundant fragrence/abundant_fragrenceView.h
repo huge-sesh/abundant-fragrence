6405cdd483b15849596ede609ac5ce8351859af1
jeff <jeff@jeffs-MacBook-Air.local>
2012-11-05 15:17:23 -0500

    Initial Commit

---
+++ abundant fragrence/abundant_fragrenceView.h
@@ -0,0 +1,13 @@
+//
+//  abundant_fragrenceView.h
+//  abundant fragrence
+//
+//  Created by jeff on 11/5/12.
+//  Copyright (c) 2012 jeff. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface abundant_fragrenceView : ScreenSaverView
+
+@end
---
+++ abundant fragrence/abundant_fragrenceView.m
@@ -0,0 +1,52 @@
+//
+//  abundant_fragrenceView.m
+//  abundant fragrence
+//
+//  Created by jeff on 11/5/12.
+//  Copyright (c) 2012 jeff. All rights reserved.
+//
+
+#import "abundant_fragrenceView.h"
+
+@implementation abundant_fragrenceView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
